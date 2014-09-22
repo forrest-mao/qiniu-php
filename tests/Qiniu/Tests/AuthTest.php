8bc76d2af053bc6929aa488899d40f0abace9b03
longbai <slongbai@gmail.com>
2014-09-22 12:43:48 +0800

    code style

--- tests/Qiniu/Tests/AuthTest.php
+++ tests/Qiniu/Tests/AuthTest.php
@@ -1,5 +1,6 @@
 <?php
 // Hack to override the time returned from the S3SignatureV4
+// @codingStandardsIgnoreStart
 namespace Qiniu\Common
 {
     function time()
@@ -13,61 +14,62 @@ namespace Qiniu\Common
 
 namespace Qiniu\Tests
 {
-use Qiniu\Common\Auth;
+    use Qiniu\Common\Auth;
+    // @codingStandardsIgnoreEnd
 
-class AuthTest extends \PHPUnit_Framework_TestCase
-{
-
-    public function testToken()
+    class AuthTest extends \PHPUnit_Framework_TestCase
     {
-        global $dummyAuth;
-        $token = $dummyAuth->token('test');
-        $this->assertEquals($token, 'abcdefghklmnopq:mSNBTR7uS2crJsyFr2Amwv1LaYg=');
-    }
 
-    public function testTokenWithData()
-    {
-        global $dummyAuth;
-        $token = $dummyAuth->tokenWithData('test');
-        $this->assertEquals($token, 'abcdefghklmnopq:-jP8eEV9v48MkYiBGs81aDxl60E=:dGVzdA==');
-    }
+        public function testToken()
+        {
+            global $dummyAuth;
+            $token = $dummyAuth->token('test');
+            $this->assertEquals($token, 'abcdefghklmnopq:mSNBTR7uS2crJsyFr2Amwv1LaYg=');
+        }
 
-    public function testTokenOfRequest()
-    {
-        global $dummyAuth;
-        $token = $dummyAuth->tokenOfRequest('http://www.qiniu.com?go=1', 'test', '');
-        $this->assertEquals($token, 'abcdefghklmnopq:cFyRVoWrE3IugPIMP5YJFTO-O-Y=');
+        public function testTokenWithData()
+        {
+            global $dummyAuth;
+            $token = $dummyAuth->tokenWithData('test');
+            $this->assertEquals($token, 'abcdefghklmnopq:-jP8eEV9v48MkYiBGs81aDxl60E=:dGVzdA==');
+        }
 
-        $token = $dummyAuth->tokenOfRequest('http://www.qiniu.com?go=1', 'test', 'application/x-www-form-urlencoded');
-        $this->assertEquals($token, 'abcdefghklmnopq:svWRNcacOE-YMsc70nuIYdaa1e4=');
-    }
+        public function testTokenOfRequest()
+        {
+            global $dummyAuth;
+            $token = $dummyAuth->tokenOfRequest('http://www.qiniu.com?go=1', 'test', '');
+            $this->assertEquals($token, 'abcdefghklmnopq:cFyRVoWrE3IugPIMP5YJFTO-O-Y=');
+            $ctype = 'application/x-www-form-urlencoded';
+            $token = $dummyAuth->tokenOfRequest('http://www.qiniu.com?go=1', 'test', $ctype);
+            $this->assertEquals($token, 'abcdefghklmnopq:svWRNcacOE-YMsc70nuIYdaa1e4=');
+        }
 
-    public function testPrivateDownloadUrl()
-    {
-        global $dummyAuth;
-        $_SERVER['override_qiniu_auth_time'] = true;
-        $url =  $dummyAuth->privateDownloadUrl('http://www.qiniu.com?go=1');
-        $expect = 'http://www.qiniu.com?go=1&e=1234571490&token=abcdefghklmnopq:8vzBeLZ9W3E4kbBLFLW0Xe0u7v4=';
-        $this->assertEquals($url, $expect);
-    }
+        public function testPrivateDownloadUrl()
+        {
+            global $dummyAuth;
+            $_SERVER['override_qiniu_auth_time'] = true;
+            $url =  $dummyAuth->privateDownloadUrl('http://www.qiniu.com?go=1');
+            $expect = 'http://www.qiniu.com?go=1&e=1234571490&token=abcdefghklmnopq:8vzBeLZ9W3E4kbBLFLW0Xe0u7v4=';
+            $this->assertEquals($url, $expect);
+        }
 
-    /**
-     * @expectedException        InvalidArgumentException
-     * @expectedExceptionMessage asyncOps has deprecated
-     */
-    public function testDeprecatedPolicy()
-    {
-        global $dummyAuth;
-        $token = $dummyAuth->uploadToken('1', null, 3600, array('asyncOps'=> 1));
-    }
+        /**
+        * @expectedException        InvalidArgumentException
+        * @expectedExceptionMessage asyncOps has deprecated
+        */
+        public function testDeprecatedPolicy()
+        {
+            global $dummyAuth;
+            $token = $dummyAuth->uploadToken('1', null, 3600, array('asyncOps'=> 1));
+        }
 
-    public function testUploadToken()
-    {
-        global $dummyAuth;
-        $_SERVER['override_qiniu_auth_time'] = true;
-        $token = $dummyAuth->uploadToken('1', '2', 3600, array('endUser'=> 'y'));
-        $expect = 'abcdefghklmnopq:sxuXJt0ObIm33J_kBT32hw0FEJo=:eyJzY29wZSI6IjEyIiwiZGVhZGxpbmUiOjEyMzQ1NzE0OTB9';
-        $this->assertEquals($token, $expect);
+        public function testUploadToken()
+        {
+            global $dummyAuth;
+            $_SERVER['override_qiniu_auth_time'] = true;
+            $token = $dummyAuth->uploadToken('1', '2', 3600, array('endUser'=> 'y'));
+            $expect = 'abcdefghklmnopq:sxuXJt0ObIm33J_kBT32hw0FEJo=:eyJzY29wZSI6IjEyIiwiZGVhZGxpbmUiOjEyMzQ1NzE0OTB9';
+            $this->assertEquals($token, $expect);
+        }
     }
-}
 }
